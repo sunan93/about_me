8561968bf2ce88649b755eb1baa927cc88761306
unknown <promitdan@gmail.com>
2021-10-02 16:47:55 +0530

    Replaced dummy content. Added photo

--- assets/scripts/tabManager.js
+++ assets/scripts/tabManager.js
@@ -40,10 +40,12 @@ function createDOMElement(type, config) {
   });
   return elem;
 }
-function populateHome() {
-  console.log('Hello');
-  const targetContainer = document.querySelector('.tabContent');
+function clearContent() {
   document.getElementsByClassName('content_wrapper')[0].remove();
+}
+function populateHome() {
+  const targetContainer = document.querySelector('.tabContent');
+  clearContent();
   const newContentWrapper = createDOMElement('div', {
     className: 'content_wrapper'
   });
@@ -51,20 +53,104 @@ function populateHome() {
     className: 'home_title',
     innerHTML: 'About me'
   });
-  const paraContent1 = `I am a PhD student in the Department of Computational & Data Sciences at the Indian Institute of Science Bangalore. Prior to this, I have worked as a Data Scientist in IBM India Pvt. Ltd after completing my Masters in Computer Science & Engineering from Indian Institute of Technology, Kharagpur.`
+  const paraContent1 = `I am a PhD Student at the Department of Computational and Data Sciences at Indian Institute of Sciences, Bengaluru. Prior to this, I did my Masters from Indian Institute of Technology, Bombay (2017-2019) in Computer Science.`
   const homePara1 = createDOMElement('p', {
     className: 'para',
     innerHTML: paraContent1
   });
+  const paraContent2 = `During my Masters, I worked on several projects in machine learning and deep learning and its applications on NLP tasks such as search and data mining, relation extraction and question answering.`
+  const homePara2 = createDOMElement('p', {
+    className: 'para',
+    innerHTML: paraContent2
+  });
+  const paraContent3 = `At Microsoft, I worked as part of the ranking, allocation and pricing team in Bing Ads.`
+  const homePara3 = createDOMElement('p', {
+    className: 'para',
+    innerHTML: paraContent3
+  });
+  const paraContent4 = `After a rich industrial experience of a year, I have joined VAL lab <a href="https://val.cds.iisc.ac.in/" target="_blank">(https://val.cds.iisc.ac.in/)</a> as a PhD student under the guidance of Prof Venkatesh Babu <a href="http://cds.iisc.ac.in/faculty/venky/" target="_blank">(http://cds.iisc.ac.in/faculty/venky/)</a>. Kindly visit my github page <a href="https://github.com/sunan93" target="_blank">(https://github.com/sunan93)</a> for more of my academic projects`
+  const homePara4 = createDOMElement('p', {
+    className: 'para',
+    innerHTML: paraContent4
+  });
   newContentWrapper.appendChild(homeTitle);
   newContentWrapper.appendChild(homePara1);
+  newContentWrapper.appendChild(homePara2);
+  newContentWrapper.appendChild(homePara3);
+  newContentWrapper.appendChild(homePara4);
   targetContainer.appendChild(newContentWrapper);
 }
 function populateAcademic() {
-  console.log('Hi');
+  clearContent();
+  const targetContainer = document.querySelector('.tabContent');
+  const newContentWrapper = createDOMElement('div', {
+    className: 'content_wrapper'
+  });
+  const academicTitle = createDOMElement('h2', {
+    className: 'academic_title',
+    innerHTML: 'Projects and Assignments'
+  });
+  const projects = [
+    {
+      title: 'Data Poisoning: DLCV Course Project',
+      link: 'https://github.com/sunan93/DLCV-Course-Project'
+    },
+    {
+      title: ' DS200 Assignment-1',
+      link: 'https://github.com/sunan93/DS200-Assignment-1'
+    },
+    {
+      title: 'Past Projects available at',
+      link: 'https://github.com/sunan93/'
+    }
+  ];
+  const listWrapper = createDOMElement('ul', {
+    className: 'project_list'
+  });
+  projects.forEach(project => {
+    const listItem = createDOMElement('li', {
+      innerHTML: `<span> ${project.title}: <a href=${project.link} target="_blank">${project.link}</a>`
+    });
+    listWrapper.appendChild(listItem);
+  });
+  newContentWrapper.appendChild(academicTitle);
+  newContentWrapper.appendChild(listWrapper);
+  targetContainer.appendChild(newContentWrapper);
 }
 function populateWorkExperience() {
-  console.log('Babli');
+  clearContent();
+  const targetContainer = document.querySelector('.tabContent');
+  const newContentWrapper = createDOMElement('div', {
+    className: 'content_wrapper'
+  });
+  const workExTitle = createDOMElement('h2', {
+    className: 'work_ex_title',
+    innerHTML: 'Past Work Experience'
+  });
+  const workExList = [
+    {
+      post: 'Research Engineer',
+      company: 'Centre for Development of Telematics',
+      duration: 'Aug 2015 to July 2017'
+    },
+    {
+      post: 'Data and Applied Scientist',
+      company: 'Microsoft',
+      duration: ' July 2019 to Aug 2020'
+    },
+  ];
+  const listWrapperWork = createDOMElement('ul', {
+    className: 'work_list'
+  });
+  workExList.forEach(workEx => {
+    const listItem = createDOMElement('li', {
+      innerHTML: `<span> <strong>${workEx.post}</strong>, <strong>${workEx.company}</strong>, ${workEx.duration}`
+    });
+    listWrapperWork.appendChild(listItem);
+  });
+  newContentWrapper.appendChild(workExTitle);
+  newContentWrapper.appendChild(listWrapperWork);
+  targetContainer.appendChild(newContentWrapper); 
 }
 function populateTabContent() {
   switch (currentTab) {
